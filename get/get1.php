3dadc83d36e196eef14b7cf53a6376b9181272c1
Jmw-Power <41883084+JMWpower@users.noreply.github.com>
2020-12-30 09:19:29 +0800

    数据来源于网络，仅供研究学习，请勿传播
    数据来源于网络，仅供研究学习，请勿分享传播

--- get/get1.php
+++ get/get1.php
@@ -1,3 +1,17 @@
 <?php
-echo 'demo.mp4';
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
+header("Access-Control-Allow-Origin: *");
+$user = "tuijian";
+$filename='../user/'.$user.'.json';
+if(file_exists($filename)){
+    $json_string = file_get_contents($filename);
+    $data = json_decode($json_string, true);
+    //查询条目数
+    $Count = count($data['url'])-1;
+    $i = mt_rand(0, $Count);
+    $url = $data['url'][$i]['url'];
+    echo $url;
+}else{
+    echo "demo.mp4";
+}
 ?>
---
+++ get/get5.php
@@ -0,0 +1,29 @@
+<?php
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
+$url = 'http://dy.yy516100.cn/dy.php';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0");
+curl_exec($ch);
+$response = curl_exec($ch);
+curl_close($ch);
+ 
+//echo $response;
+
+$need=getNeedBetween($response, '"http://' , '" width=' );
+
+echo $need;
+
+function getNeedBetween($kw1,$mark1,$mark2){
+$kw=$kw1;
+$kw='123'.$kw.'123';
+$st =stripos($kw,$mark1);
+$ed =stripos($kw,$mark2);
+if(($st==false||$ed==false)||$st>=$ed)
+return 0;
+$kw=substr($kw,($st+1),($ed-$st-1));
+return $kw;
+}
+?>
